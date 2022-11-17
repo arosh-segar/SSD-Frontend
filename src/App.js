a13ae6dc817304028f65c1a9b3e9075b6e317b11
arosh-segar <arosh.segar@gmail.com>
2022-11-17 12:59:56 +0530

    admin panel

--- src/App.js
+++ src/App.js
@@ -1,63 +1,70 @@
 import React from "react";
 import keycloak from "./helperFiles/Keycloak";
-import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
-import {ReactKeycloakProvider} from "@react-keycloak/web";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
 import PrivateRoute from "./helperFiles/PrivateRoute";
 import ViewMessagesPage from "./pages/view-messages/ViewMessagesPage";
 import ViewFilesPage from "./pages/view-files/ViewFilesPage";
-import {apiInstance} from "./apis/apiInstance"
+import { apiInstance } from "./apis/apiInstance";
+import ViewUsersPage from "./pages/view-users/ViewUsersPage";
+import DashboardPage from "./pages/dashboard/DashboardPage";
 
 function App() {
-    const eventLogger = (e) => {
-        const user = keycloak.idTokenParsed.name;
-        console.log(user);
-    };
+  const eventLogger = (e) => {
+    const user = keycloak.idTokenParsed.name;
+    console.log(user);
+  };
 
-    const tokenLogger = (e) => {
-        /* console.log(e); */
-    };
-    return (
-        <div>
-            <ReactKeycloakProvider
-                authClient={keycloak}
-                initOptions={{onLoad: "login-required"}}
-                onEvent={eventLogger}
-                onTokens={tokenLogger}
-            >
-                <BrowserRouter>
-                    <Routes>
-                        <Route
-                            exact
-                            path="/"
-                            element={
-                                <Navigate replace to="/messages">
-                                    <PrivateRoute>
-                                        <ViewMessagesPage/>
-                                    </PrivateRoute>
-                                </Navigate>
-                            }
-                        />
-                        <Route
-                            path="/messages"
-                            element={
-                                <PrivateRoute>
-                                    <ViewMessagesPage/>
-                                </PrivateRoute>
-                            }
-                        />
-                        <Route
-                            path="/files"
-                            element={
-                                <PrivateRoute>
-                                    <ViewFilesPage/>
-                                </PrivateRoute>
-                            }
-                        />
-                    </Routes>
-                </BrowserRouter>
-            </ReactKeycloakProvider>
-        </div>
-    );
+  const tokenLogger = (e) => {
+    /* console.log(e); */
+  };
+  return (
+    <div>
+      <ReactKeycloakProvider
+        authClient={keycloak}
+        initOptions={{ onLoad: "login-required" }}
+        onEvent={eventLogger}
+        onTokens={tokenLogger}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <DashboardPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/messages"
+              element={
+                <PrivateRoute>
+                  <ViewMessagesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/files"
+              element={
+                <PrivateRoute>
+                  <ViewFilesPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/users"
+              element={
+                <PrivateRoute>
+                  <ViewUsersPage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ReactKeycloakProvider>
+    </div>
+  );
 }
 
 export default App;
